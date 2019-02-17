4dbe4d626c582f6919f85e99c6655ec2f8e20c16
Mark Chen <fzmwczl@gmail.com>
2019-02-18 00:29:28 +0800

    - bug修复:   - 修正了一处typo
    + 新增功能：
      + 新增将剧情BGM单独分类保存至wav\story_bgm的功能

--- 生成.bat
+++ 生成.bat
@@ -1,5 +1,5 @@
-::设置活动代码页为UFT-8
 @echo off
+::设置活动代码页为UTF-8
 chcp 65001
 
 ::对于fsb\当中以.fsb为拓展名的文件进行提取
@@ -12,7 +12,15 @@ if exist wav (
 	mkdir wav
 	goto clean
 ::将提取出的wav文件移动至wav文件夹
+
 :clean
+	::含有[0-9]*-[0-9]*为剧情BGM，但是其中乱入了一个fx-zhujue01_skill1004_3.wav
+	::这里我真的想吐槽一下网易的音频命名
+	::有拼音有罗马音还有缩写
+	::命名不规范调用两行泪知道吗
+	mkdir wav\story_bgm
+		move /y *-*.wav wav\story_bgm
+		move /y wav\story_bgm\fx-zhujue*.wav wav
 	move /Y *.wav wav
 	::del /S /Q fsb\*.fsb
 	echo 操作完成！文件已保存wav文件夹中
