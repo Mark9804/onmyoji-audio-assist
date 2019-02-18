80c76c41b4d82c8e8b06dfd4fd5ebffc080311ad
Mark Chen <fzmwczl@gmail.com>
2019-02-18 12:09:46 +0800

    bug fixed & feature - bug修复：   - 解决了清理代码当中的很多bug（我也很奇怪同一个语法有的能用有的不能用）   - 修复typo + 新增功能   + 直接从默认安装目录当中提取   + 添加对安倍晴明部分语音的分类支持（应该还有其他内容，几千个文件找起来挺麻烦的）

--- 生成.bat
+++ 生成.bat
@@ -2,14 +2,19 @@
 ::设置活动代码页为UTF-8
 chcp 65001
 
+::从yys默认安装路径当中提取*.fsb
+if not exist fsb (
+	mkdir fsb
+	)
+copy "C:\Program Files (x86)\Onmyoji\res\sound\output\*.fsb" fsb\*.fsb
+
 ::对于fsb\当中以.fsb为拓展名的文件进行提取
 for /f %%i in ('dir /b fsb\*.fsb') do fmodex_extractor.exe fsb\%%i
 
 ::判断是否存在wav文件夹，若存在则执行清理，不存在则创建后清理
-if exist wav (
-	goto clean
-	)
+if not exist wav (
 	mkdir wav
+	)
 	goto clean
 ::将提取出的wav文件移动至wav文件夹
 
@@ -19,9 +24,15 @@ if exist wav (
 	::有拼音有罗马音还有缩写
 	::命名不规范调用两行泪知道吗
 	mkdir wav\story_bgm
-		move /y *-*.wav wav\story_bgm
-		move /y wav\story_bgm\fx-zhujue*.wav wav
-	move /Y *.wav wav
-	::del /S /Q fsb\*.fsb
-	echo 操作完成！文件已保存wav文件夹中
+		move /y *-*.wav wav\story_bgm\
+		move /y wav\story_bgm\fx-zhujue*.wav wav\
+    ::48*应该是剧情里的sfx
+    mkdir wav\story_sfx
+		move /y 48*.wav wav\story_sfx\
+    ::安倍晴明的语音文件名是AbenoSeimei*
+    mkdir wav\AbenoSeimei安倍晴明
+        move /y AbenoSeimei*.wav wav\AbenoSeimei安倍晴明\
+	move /Y *.wav wav\
+	del /S /Q fsb\*.fsb
+	echo 操作完成！文件已保存在wav文件夹中
 	pause
